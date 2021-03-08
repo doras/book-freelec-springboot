f3c08a70fdbb3bbc857367467ac823e7a5050bc8
doras <shoon0113@gmail.com>
2021-03-08 15:08:14 +0900

    upto 3.5 (JPA Auditing - createDate, modifiedDate)

--- src/main/java/com/doras/tutorials/springboot/domain/posts/Posts.java
+++ src/main/java/com/doras/tutorials/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.doras.tutorials.springboot.domain.posts;
 
+import com.doras.tutorials.springboot.domain.BaseTimeEntity;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -14,7 +15,7 @@ import javax.persistence.Id;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
