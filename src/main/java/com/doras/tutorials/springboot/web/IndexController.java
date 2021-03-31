ddbc1155c85546fab31ac6dbab180ecd834cc1f4
doras <shoon0113@gmail.com>
2021-04-01 07:16:17 +0900

    upto 5.4 (add annotation LoginUser, argument resolver)

--- src/main/java/com/doras/tutorials/springboot/web/IndexController.java
+++ src/main/java/com/doras/tutorials/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.doras.tutorials.springboot.web;
 
+import com.doras.tutorials.springboot.config.auth.LoginUser;
 import com.doras.tutorials.springboot.config.auth.dto.SessionUser;
 import com.doras.tutorials.springboot.service.posts.PostsService;
 import com.doras.tutorials.springboot.web.dto.PostsResponseDto;
@@ -19,10 +20,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
 
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
