5363e30f650d4cbb259ef17ed24dd9ff7acce439
doras <shoon0113@gmail.com>
2021-04-01 09:15:04 +0900

    upto 5.6 (add naver social login)

--- src/main/java/com/doras/tutorials/springboot/web/IndexController.java
+++ src/main/java/com/doras/tutorials/springboot/web/IndexController.java
@@ -24,7 +24,7 @@ public class IndexController {
         model.addAttribute("posts", postsService.findAllDesc());
 
         if (user != null) {
-            model.addAttribute("userName", user.getName());
+            model.addAttribute("userNames", user.getName());
         }
 
         return "index";
