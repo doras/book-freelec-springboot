1d219f26a726111f12277ed00015667cc446b965
doras <shoon0113@gmail.com>
2021-04-06 05:16:13 +0900

    upto 5.7 (apply security to unit test)

--- src/test/java/com/doras/tutorials/springboot/web/HelloControllerTest.java
+++ src/test/java/com/doras/tutorials/springboot/web/HelloControllerTest.java
@@ -1,11 +1,15 @@
 package com.doras.tutorials.springboot.web;
 
 
+import com.doras.tutorials.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -17,12 +21,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        }
+)
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
+    @WithMockUser(roles = "USER")
     @Test
     public void hello_will_be_return() throws Exception
     {
@@ -33,6 +42,7 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));
     }
 
+    @WithMockUser(roles = "USER")
     @Test
     public void helloDto_will_return() throws Exception
     {
